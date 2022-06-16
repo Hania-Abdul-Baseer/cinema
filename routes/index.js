e753a1cb98b1879996a44a6ab654bc5f138f65e9
Hania Abdul Baseer <76831541+Hania-Abdul-Baseer@users.noreply.github.com>
2022-06-16 14:14:09 +0000

    reusing index.js from sample exam

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,57 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// GET request that returns the name and price of all the shoes that match the user's search and filter
+router.get('/shoes', function(req, res, next) {
+  // this is a function that is going to run asynchronously and it will execute the inner function once it establishes a connection with the database
+  // this way the database is not always connected and only when its needed for retrieval of data
+  req.pool.getConnection(function(error, connection){
+    if(error){
+      console.log("error line 15");
+      res.sendStatus(500);
+      return;
+    }
+
+
+    let query = "SELECT shoe_name, price FROM shoes;";
+    connection.query(query, function(error, rows, fields){
+      connection.release(); // release connection
+      if(error){
+        console.log("error line 25 - index.js");
+        res.sendStatus(500);
+        return;
+      }
+      res.json(rows); // send response
+      console.log("ok line 30 - index.js");
+    });
+  });
+});
+
+/*
+// GET to query all the actors in the database and display them in the html table, everytime the webapp loads
+router.get('/actors', function(req, res) {
+  // this is a function that is going to run asynchronously and it will execute the inner function once it establishes a connection with the database
+  // this way the database is not always connected and only when its needed for retrieval of data
+  //
+  req.pool.getConnection(function(error, connection){
+    if(error){
+      console.log("error line 16");
+      res.sendStatus(500);
+      return;
+    }
+    var query = "SELECT first_name, last_name FROM actor;";
+    connection.query(query, function(error, rows, fields){
+      connection.release(); // release connection
+      if(error){
+        console.log("error line 24");
+        res.sendStatus(500);
+        return;
+      }
+      res.json(rows); // send response
+    });
+
+  });
+});
+*/
+
 module.exports = router;
