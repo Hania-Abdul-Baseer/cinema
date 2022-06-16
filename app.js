19a046d6a1464785b35cd7c78a54ecec4411de93
Hania Abdul Baseer <76831541+Hania-Abdul-Baseer@users.noreply.github.com>
2022-06-16 14:04:52 +0000

    f

--- app.js
+++ app.js
@@ -4,10 +4,27 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// this line is required to attach a session to all requests
+var session = require('express-session');
+
+// for integerating mysql db to the webapp
+var mysql = require('mysql');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// creating the dbconnectionpool to integerate sql database into the webapp
+var dbConnectionPool = mysql.createPool({
+  host: 'localhost',
+  database: 'task1_3'
+});
+
+app.use(function(req, res, next) {
+  req.pool = dbConnectionPool;
+  next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,6 +37,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
+// this ensures that all user related requests ie requests that begin with /users is handles from user.js
+// this is just to split the middlewares from having it all in app.js to having some in app.js and some in users.js
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
